1e99a8adfcc2ff706d1773b6174f32a2b4ee9cca
Junaid Mehmood <164566813+JunaiidAhmadd@users.noreply.github.com>
2025-12-16 12:38:51 +0500

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,23 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("Mongo Error:", err));
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URL, {
+      bufferCommands: false,   // VERY IMPORTANT
+      serverSelectionTimeoutMS: 10000,
+    });
+
+    isConnected = db.connections[0].readyState === 1;
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+    throw err;
+  }
+};
+
+module.exports = connectDB;
+
